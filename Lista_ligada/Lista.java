75ce049b0347a755243c4fbd910db53fee4bcc45
Nicole Lira <nicksenju03@gmail.com>
2026-06-19 18:23:35 -0300

    Create Lista.java

---
+++ Lista_ligada/Lista.java
@@ -0,0 +1,58 @@
+package lista3_compoe_p3.lista_ligada;
+
+public class Lista {
+    private No primeiro;
+    //construtor é o padrão
+    public boolean listaVazia() {
+        return primeiro == null;
+    }
+    public void insereInicio(String info) {
+        No novo = new No(info);
+        if (!listaVazia()) {
+            novo.setProximo(primeiro);
+        }
+        primeiro = novo;
+    }
+    
+    public void insereFim (String info) {
+        No novo = new No(info);
+        if (listaVazia())
+            primeiro = novo;
+        else {
+            No runner = primeiro;
+            while (runner.getProximo() != null) {
+                runner = runner.getProximo();
+            }
+            runner.setProximo(novo);
+        }
+    }
+    public String removeFim() {
+        if (listaVazia()){
+            return "biblioteca vazia";
+        }
+        else if (primeiro.getProximo() == null) { //tem um só
+            String livro = primeiro.getInfo();
+            primeiro = null; //esvaziou a lista
+            return livro;
+        }
+        No runner = primeiro;
+        while (runner.getProximo().getProximo() != null){
+            runner = runner.getProximo();
+        }
+        String livro = runner.getProximo().getInfo();
+        runner.setProximo(null);
+        return livro;
+    }
+    @Override
+    public String toString() {
+        if (listaVazia()) return "lista vazia";
+        String s = "";
+        No runner = primeiro;
+        while (runner != null) { //vai até o fim da lista
+            s += runner.getInfo() + "->";
+            runner = runner.getProximo();
+        }
+        return s + "//";
+    }
+}
+
